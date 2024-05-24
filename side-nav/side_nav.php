63707f3eab3696ebf12ceddf629acb08c6d4661a
Testing <bassoelibariki56@gmail.com>
2024-05-24 17:50:22 +0300

    initializing the project (template customization)

---
+++ side-nav/side_nav.php
@@ -0,0 +1,191 @@
+<div class="collapse navbar-collapse" id="navbarVerticalCollapse">
+          <!-- scrollbar removed-->
+          <div class="navbar-vertical-content">
+            <ul class="navbar-nav flex-column" id="navbarVerticalNav">
+            <li class="nav-item">
+                <!-- parent pages-->
+                
+                  <div class="nav-item-wrapper"><a class="nav-link label-1" href="index.php" role="button" data-bs-toggle="" aria-expanded="false">
+                    <div class="d-flex align-items-center"><span class="nav-link-icon"><span data-feather="pie-chart"></span></span><span class="nav-link-text-wrapper"><span class="nav-link-text">Dashboard</span></span></div>
+                  </a></div><!-- parent pages-->
+               
+              </li>
+              <li class="nav-item">
+                <!-- label-->
+                <p class="navbar-vertical-label">Apps</p>
+                <hr class="navbar-vertical-line" /><!-- parent pages-->
+                <div class="nav-item-wrapper">
+
+                <div class="nav-item-wrapper"><a class="nav-link dropdown-indicator label-1" href="#nv-project-management" role="button" data-bs-toggle="collapse" aria-expanded="false" aria-controls="nv-project-management">
+                    <div class="d-flex align-items-center">
+                      <div class="dropdown-indicator-icon"><span class="fas fa-caret-right"></span></div><span class="nav-link-icon"><span data-feather="clipboard"></span></span><span class="nav-link-text">Project management</span>
+                    </div>
+                  </a>
+                  <div class="parent-wrapper label-1">
+                    <ul class="nav collapse parent " data-bs-parent="#navbarVerticalCollapse" id="nv-project-management">
+                      <!-- <li class="collapsed-nav-item-title d-none">Project management</li> -->
+                      <li class="nav-item"><a class="nav-link" href="create-new.php" data-bs-toggle="" aria-expanded="false">
+                          <div class="d-flex align-items-center"><span class="nav-link-text">Project type</span></div>
+                        </a><!-- more inner pages-->
+                      </li>
+                      <li class="nav-item"><a class="nav-link" href="create-new.php" data-bs-toggle="" aria-expanded="false">
+                          <div class="d-flex align-items-center"><span class="nav-link-text">Project category</span></div>
+                        </a><!-- more inner pages-->
+                      </li>
+                      <li class="nav-item"><a class="nav-link" href="create-new.php" data-bs-toggle="" aria-expanded="false">
+                          <div class="d-flex align-items-center"><span class="nav-link-text">Create new</span></div>
+                        </a><!-- more inner pages-->
+                      </li>
+                      <li class="nav-item"><a class="nav-link" href="create-new.php" data-bs-toggle="" aria-expanded="false">
+                          <div class="d-flex align-items-center"><span class="nav-link-text">Phases</span></div>
+                        </a><!-- more inner pages-->
+                      </li>
+                      <li class="nav-item"><a class="nav-link" href="create-new.php" data-bs-toggle="" aria-expanded="false">
+                          <div class="d-flex align-items-center"><span class="nav-link-text">Deliverable</span></div>
+                        </a><!-- more inner pages-->
+                      </li>
+                      
+                      <li class="nav-item"><a class="nav-link active" href="project-list-view.php" data-bs-toggle="" aria-expanded="false">
+                          <div class="d-flex align-items-center"><span class="nav-link-text">Project list view</span></div>
+                        </a><!-- more inner pages-->
+                      </li>
+                      
+                      <li class="nav-item"><a class="nav-link" href="todo-list.php" data-bs-toggle="" aria-expanded="false">
+                          <div class="d-flex align-items-center"><span class="nav-link-text">Todo list</span></div>
+                        </a><!-- more inner pages-->
+                      </li>
+                      <li class="nav-item"><a class="nav-link" href="project-details.php" data-bs-toggle="" aria-expanded="false">
+                          <div class="d-flex align-items-center"><span class="nav-link-text">Project details</span></div>
+                        </a><!-- more inner pages-->
+                      </li>
+                      <li class="nav-item"><a class="nav-link" href="create-new.php" data-bs-toggle="" aria-expanded="false">
+                          <div class="d-flex align-items-center"><span class="nav-link-text">Resources</span></div>
+                        </a><!-- more inner pages-->
+                      </li>
+                      <li class="nav-item"><a class="nav-link" href="create-new.php" data-bs-toggle="" aria-expanded="false">
+                          <div class="d-flex align-items-center"><span class="nav-link-text">Constant budget</span></div>
+                        </a><!-- more inner pages-->
+                      </li>
+                      <li class="nav-item"><a class="nav-link" href="project-details.php" data-bs-toggle="" aria-expanded="false">
+                          <div class="d-flex align-items-center"><span class="nav-link-text">Upload project</span></div>
+                        </a><!-- more inner pages-->
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+                
+                <!-- User management -->
+                <div class="nav-item-wrapper"><a class="nav-link dropdown-indicator label-1" href="#nv-user-management" role="button" data-bs-toggle="collapse" aria-expanded="false" aria-controls="nv-user-management">
+                    <div class="d-flex align-items-center">
+                      <div class="dropdown-indicator-icon"><span class="fas fa-caret-right"></span></div><span class="nav-link-icon"><span data-feather="user"></span></span><span class="nav-link-text">User management</span>
+                    </div>
+                  </a>
+                  <div class="parent-wrapper label-1">
+                    <ul class="nav collapse parent" data-bs-parent="#navbarVerticalCollapse" id="nv-user-management">
+                      <li class="collapsed-nav-item-title d-none">User management</li>
+                      <li class="nav-item"><a class="nav-link" href="create-new.php" data-bs-toggle="collapse" aria-expanded="false">
+                          <div class="d-flex align-items-center"><span class="nav-link-text">User list</span></div>
+                        </a><!-- more inner pages-->
+                      </li>
+                      <li class="nav-item"><a class="nav-link" href="create-new.php" data-bs-toggle="collapse" aria-expanded="false">
+                          <div class="d-flex align-items-center"><span class="nav-link-text">Role list</span></div>
+                        </a><!-- more inner pages-->
+                      </li>
+                    
+                    </ul>
+                  </div>
+                </div>
+
+               <!-- Members -->
+               <div class="nav-item-wrapper"><a class="nav-link label-1" href="chat.php" role="button" data-bs-toggle="" aria-expanded="false">
+                    <div class="d-flex align-items-center"><span class="nav-link-icon"><span data-feather="users"></span></span><span class="nav-link-text-wrapper"><span class="nav-link-text">Team</span></span></div>
+                  </a>
+
+                  <!-- Charts -->
+                <div class="nav-item-wrapper"><a class="nav-link label-1" href="chat.php" role="button" data-bs-toggle="" aria-expanded="false">
+                    <div class="d-flex align-items-center"><span class="nav-link-icon"><span data-feather="message-square"></span></span><span class="nav-link-text-wrapper"><span class="nav-link-text">Chat</span></span></div>
+                  </a></div><!-- parent pages-->    
+              
+                  <!-- Calendar -->
+                <div class="nav-item-wrapper"><a class="nav-link label-1" href="calendar.php" role="button" data-bs-toggle="" aria-expanded="false">
+                    <div class="d-flex align-items-center"><span class="nav-link-icon"><span data-feather="calendar"></span></span><span class="nav-link-text-wrapper"><span class="nav-link-text">Calendar</span></span></div>
+                  </a></div>
+              </li>
+             
+              <li class="nav-item">
+                <!-- label-->
+                <p class="navbar-vertical-label">Documentation</p>
+                <hr class="navbar-vertical-line" /><!-- parent pages-->                 
+                <div class="nav-item-wrapper"><a class="nav-link dropdown-indicator label-1" href="#nv-report" role="button" data-bs-toggle="collapse" aria-expanded="false" aria-controls="nv-report">
+                    <div class="d-flex align-items-center">
+                      <div class="dropdown-indicator-icon"><span class="fas fa-caret-right"></span></div><span class="nav-link-icon"><span data-feather="file-text"></span></span><span class="nav-link-text">Report</span>
+                    </div>
+                  </a>
+                  <div class="parent-wrapper label-1">
+                    <ul class="nav collapse parent" data-bs-parent="#navbarVerticalCollapse" id="nv-report">
+                      <li class="collapsed-nav-item-title d-none">Customization</li>
+                      <li class="nav-item"><a class="nav-link" href="../../documentation/customization/configuration.php" data-bs-toggle="" aria-expanded="false">
+                          <div class="d-flex align-items-center"><span class="nav-link-text">General report</span></div>
+                        </a><!-- more inner pages-->
+                      </li>
+                      <li class="nav-item"><a class="nav-link" href="../../documentation/customization/styling.php" data-bs-toggle="" aria-expanded="false">
+                          <div class="d-flex align-items-center"><span class="nav-link-text">Individual reporrt</span></div>
+                        </a><!-- more inner pages-->
+                      </li>
+                     
+                    </ul>
+                  </div>
+                </div><!-- parent pages-->
+
+                <!-- Settings -->
+                <div class="nav-item-wrapper"><a class="nav-link dropdown-indicator label-1" href="#nv-settings" role="button" data-bs-toggle="collapse" aria-expanded="false" aria-controls="nv-settings">
+                    <div class="d-flex align-items-center">
+                      <div class="dropdown-indicator-icon"><span class="fas fa-caret-right"></span></div><span class="nav-link-icon"><span data-feather="settings"></span></span><span class="nav-link-text">Settings</span>
+                    </div>
+                  </a>
+                  <div class="parent-wrapper label-1">
+                    <ul class="nav collapse parent" data-bs-parent="#navbarVerticalCollapse" id="nv-settings">
+                      <li class="collapsed-nav-item-title d-none">Settings</li>
+                      <li class="nav-item"><a class="nav-link" href="../../documentation/customization/configuration.php" data-bs-toggle="" aria-expanded="false">
+                          <div class="d-flex align-items-center"><span class="nav-link-text">Role</span></div>
+                        </a><!-- more inner pages-->
+                      </li>
+                      <li class="nav-item"><a class="nav-link" href="../../documentation/customization/styling.php" data-bs-toggle="" aria-expanded="false">
+                          <div class="d-flex align-items-center"><span class="nav-link-text">Permission</span></div>
+                        </a><!-- more inner pages-->
+                      </li>
+                    </ul>
+                  </div>
+                </div><!-- parent pages-->
+                
+                <div class="nav-item-wrapper">
+                  <div class="parent-wrapper label-1">
+                    <ul class="nav collapse parent" data-bs-parent="#navbarVerticalCollapse" id="nv-layouts-doc">
+                      <li class="collapsed-nav-item-title d-none">Layouts doc</li>
+                      <li class="nav-item"><a class="nav-link" href="../../documentation/layouts/vertical-navbar.php" data-bs-toggle="" aria-expanded="false">
+                          <div class="d-flex align-items-center"><span class="nav-link-text">Vertical navbar</span></div>
+                        </a><!-- more inner pages-->
+                      </li>
+                      <li class="nav-item"><a class="nav-link" href="../../documentation/layouts/horizontal-navbar.php" data-bs-toggle="" aria-expanded="false">
+                          <div class="d-flex align-items-center"><span class="nav-link-text">Horizontal navbar</span></div>
+                        </a><!-- more inner pages-->
+                      </li>
+                      <li class="nav-item"><a class="nav-link" href="../../documentation/layouts/combo-navbar.php" data-bs-toggle="" aria-expanded="false">
+                          <div class="d-flex align-items-center"><span class="nav-link-text">Combo navbar</span></div>
+                        </a><!-- more inner pages-->
+                      </li>
+                      <li class="nav-item"><a class="nav-link" href="../../documentation/layouts/dual-nav.php" data-bs-toggle="" aria-expanded="false">
+                          <div class="d-flex align-items-center"><span class="nav-link-text">Dual nav</span></div>
+                        </a><!-- more inner pages-->
+                      </li>
+                    </ul>
+                  </div>
+                </div><!-- parent pages-->
+               
+                <div class="nav-item-wrapper"><a class="nav-link label-1" href="../../showcase.php" role="button" data-bs-toggle="" aria-expanded="false">
+                    <div class="d-flex align-items-center"><span class="nav-link-icon"><span data-feather="monitor"></span></span><span class="nav-link-text-wrapper"><span class="nav-link-text">Showcase</span></span></div>
+                  </a></div>
+              </li>
+            </ul>
+          </div>
+        </div>
